ed6764cd01ab1b137c6f61caa07c49b014039e54
Nick Todts <nick-todts@hotmail.com>
2020-01-20 10:54:43 +0100

    Import arcade graphics library. Draw board game and draw an X and an O object on the board.

--- app.py
+++ app.py
@@ -1,8 +1,68 @@
+import arcade
+
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 600
+SCREEN_TITLE = "Tic Tac Toe"
+
+
+class X:
+    def __init__(self, x, y):
+        self.x = x  # center coordinate x
+        self.y = y  # center coordinate y
+        self.size = 50
+
+    def draw(self):
+        arcade.draw_line(self.x - self.size, self.y - self.size, self.x + self.size, self.y + self.size, arcade.color.WHITE, 4)
+        arcade.draw_line(self.x - self.size, self.y + self.size, self.x + self.size, self.y - self.size, arcade.color.WHITE, 4)
+
+
+class O:
+    def __init__(self, x, y):
+        self.x = x  # center coordinate x
+        self.y = y  # center coordinate y
+        self.radius = 50
+
+    def draw(self):
+        arcade.draw_circle_outline(self.x, self.y, self.radius, arcade.color.WHITE, 4)
+
+
+class MyGameWindow(arcade.Window):
+    def __init__(self, width, height, title):
+        super().__init__(width, height, title)
+        self.set_location(100, 100)
+        arcade.set_background_color(arcade.color.BLACK)
+
+    def draw_board(self):
+        arcade.draw_lines([(200, 0), (200, 600)], arcade.color.WHITE, 3)
+        arcade.draw_lines([(400, 0), (400, 600)], arcade.color.WHITE, 3)
+        arcade.draw_lines([(0, 200), (600, 200)], arcade.color.WHITE, 3)
+        arcade.draw_lines([(0, 400), (600, 400)], arcade.color.WHITE, 3)
+
+    def on_draw(self):
+        arcade.start_render()
+        self.draw_board()
+        obj_x = X(100, 100)
+        obj_x.draw()
+        obj_o = O(100, 300)
+        obj_o.draw()
+
+    def on_update(self, delta_time: float):
+        pass
+
+    def on_key_release(self, symbol: int, modifiers: int):
+        if symbol == arcade.key.ESCAPE or symbol == arcade.key.Q:
+            pass
+
+    def on_mouse_press(self, x: float, y: float, button: int, modifiers: int):
+        if button == arcade.MOUSE_BUTTON_LEFT:
+            pass
+
+
 def menu():
     choice = input('''
 ----- Tic Tac Toe -----
 | 0: Play with CLI     |
-| 1: Play Toe with GUI |
+| 1: Play with GUI |
 | 2: exit              |
 |----------------------|\n''')
     return choice
@@ -105,15 +165,25 @@ def tictactoe_cli():
     show_game_result(game_result)
 
 
+def tictactoe_gui():
+    MyGameWindow(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    x = X(100, 100)
+    x.draw()
+    arcade.run()  # Run game window until user clicks on exit.
+
+
 def main():
     while True:
         choice = menu()
+        #choice = '1'  # quicker for development
 
         # Execute chosen menu option.
         if choice == '0':
             tictactoe_cli()
+            break
         elif choice == '1':
-            pass
+            tictactoe_gui()
+            break
         elif choice == '2':
             print("Exiting program")
             break
