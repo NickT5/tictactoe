8c9a5288b5029bcdc8137fe29d58acaaf6d06b0e
Nick Todts <nick-todts@hotmail.com>
2020-01-22 11:21:11 +0100

    Add graphics for TicTacToe (core gameplay is implemented atm). Next step is to add game states and refactor code.

--- app.py
+++ app.py
@@ -1,50 +1,109 @@
 import arcade
+from Player import X, O
 
 SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 600
 SCREEN_TITLE = "Tic Tac Toe"
 
 
-class X:
-    def __init__(self, x, y):
-        self.x = x  # center coordinate x
-        self.y = y  # center coordinate y
-        self.size = 50
-
-    def draw(self):
-        arcade.draw_line(self.x - self.size, self.y - self.size, self.x + self.size, self.y + self.size, arcade.color.WHITE, 4)
-        arcade.draw_line(self.x - self.size, self.y + self.size, self.x + self.size, self.y - self.size, arcade.color.WHITE, 4)
-
-
-class O:
-    def __init__(self, x, y):
-        self.x = x  # center coordinate x
-        self.y = y  # center coordinate y
-        self.radius = 50
-
-    def draw(self):
-        arcade.draw_circle_outline(self.x, self.y, self.radius, arcade.color.WHITE, 4)
-
-
-class MyGameWindow(arcade.Window):
+class MyGame(arcade.Window):
     def __init__(self, width, height, title):
         super().__init__(width, height, title)
+        self.board = [['-', '-', '-'],
+                      ['-', '-', '-'],
+                      ['-', '-', '-']]
+        self.players = ['X', 'O']
+        self.current_turn = self.players[0]
+        self.next_turn = self.players[1]
+        self.x_list = []  # location of all X objects.
+        self.o_list = []  # location of all O objects.
+        self.center_locations = [(100, 500), (300, 500), (500, 500),
+                                 (100, 300), (300, 300), (500, 300),
+                                 (100, 100), (300, 100), (500, 100)]
         self.set_location(100, 100)
         arcade.set_background_color(arcade.color.BLACK)
 
-    def draw_board(self):
+    def show_board_data(self):
+        for row in self.board:
+            print(f"|{row[0]}|{row[1]}|{row[2]}|")
+
+    def switch_turn(self):
+        self.current_turn, self.next_turn = self.next_turn, self.current_turn
+
+    def draw_board_grid(self):
+        # todo refactor this so it's in function of the width, height of the game.
         arcade.draw_lines([(200, 0), (200, 600)], arcade.color.WHITE, 3)
         arcade.draw_lines([(400, 0), (400, 600)], arcade.color.WHITE, 3)
         arcade.draw_lines([(0, 200), (600, 200)], arcade.color.WHITE, 3)
         arcade.draw_lines([(0, 400), (600, 400)], arcade.color.WHITE, 3)
 
+    def get_location_clicked(self, x, y):
+        # todo refactor this. Try x/boxsize, y/boxsize
+        # First row
+        if 0 <= x < 200 and 400 <= y <= 600:
+            return 1
+        elif 200 <= x < 400 and 400 <= y <= 600:
+            return 2
+        elif 400 <= x <= 600 and 400 <= y <= 600:
+            return 3
+        # Second row
+        elif 0 <= x < 200 and 200 <= y < 400:
+            return 4
+        elif 200 <= x < 400 and 200 <= y < 400:
+            return 5
+        elif 400 <= x <= 600 and 200 <= y < 400:
+            return 6
+        # Third row
+        elif 0 <= x < 200 and 0 <= y < 200:
+            return 7
+        elif 200 <= x < 400 and 0 <= y < 200:
+            return 8
+        elif 400 <= x <= 600 and 0 <= y < 200:
+            return 9
+        else:
+            return -1
+
+    def check_if_winner(self):
+        b = self.board
+        # Check horizontal
+        for h in range(3):
+            if b[h][0] == b[h][1] == b[h][2] and b[h][0] != '-':
+                return True
+        # Check vertical
+        for v in range(3):
+            if b[0][v] == b[1][v] == b[2][v] and b[0][v] != '-':
+                return True
+        # Check diagonal
+        if (b[0][0] == b[1][1] == b[2][2] and b[0][0] != '-') or (b[2][0] == b[1][1] == b[0][2] and b[2][0] != '-'):
+            return True
+
+    def is_board_full(self):
+        for row in range(3):
+            for col in range(3):
+                if self.board[row][col] == '-':
+                    return False
+        return True
+
+    def is_game_done(self):
+        # Game is over if there's a winner or the board is full (= a tie).
+        if self.check_if_winner():
+            print(f"{self.next_turn} won!")
+            return True
+        elif self.is_board_full():
+            print("It's a tie.")
+            return True
+        else:
+            return False
+
     def on_draw(self):
         arcade.start_render()
-        self.draw_board()
-        obj_x = X(100, 100)
-        obj_x.draw()
-        obj_o = O(100, 300)
-        obj_o.draw()
+        self.draw_board_grid()
+        # Draw the X's.
+        for x in self.x_list:
+            x.draw()
+        # Draw the O's.
+        for o in self.o_list:
+            o.draw()
 
     def on_update(self, delta_time: float):
         pass
@@ -55,7 +114,27 @@ class MyGameWindow(arcade.Window):
 
     def on_mouse_press(self, x: float, y: float, button: int, modifiers: int):
         if button == arcade.MOUSE_BUTTON_LEFT:
-            pass
+            location = self.get_location_clicked(x, y)
+            print(f"({x},{y}) You clicked in {location}")
+
+            # Add X or O to the correct board location if it's not filled.
+            location -= 1  # Conversion from user logic to array indexing logic.
+            row = location // 3
+            col = location % 3
+            if self.board[row][col] == 'X' or self.board[row][col] == 'O':
+                print("Box already filled! Please choose another location.")
+            else:
+                self.board[row][col] = self.current_turn
+                cx, cy = self.center_locations[location]
+                if self.current_turn == 'X':
+                    self.x_list.append(X(cx, cy))
+                else:
+                    self.o_list.append(O(cx, cy))
+                self.switch_turn()
+
+            self.show_board_data()
+            if self.is_game_done():
+                self.close()        # Close window if game is over.
 
 
 def menu():
@@ -98,6 +177,7 @@ def init_turns(players):
 
 
 def check_if_winner(b):
+     # todo: possible to code this more efficient by only check the rows/cols/diagonals of the last move.
     is_over = False
     # Check horizontal
     for h in range(3):
@@ -166,9 +246,7 @@ def tictactoe_cli():
 
 
 def tictactoe_gui():
-    MyGameWindow(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
-    x = X(100, 100)
-    x.draw()
+    MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
     arcade.run()  # Run game window until user clicks on exit.
 
 
